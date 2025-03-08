b76334362ae68a408fe234f04b8acbef84d6b634
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-03-08 08:30:06 +0200

    Delete tests directory

--- tests/app.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('../app');
-
-describe('GET /', () => {
-  it('should return a welcome message', async () => {
-    const response = await request(app).get('/');
-    expect(response.text).toBe('Welcome to the API');
-  });
-});
