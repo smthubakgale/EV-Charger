514f2dcfa50c65d3374556bc37b20603a3c5688f
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-03-08 08:29:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const websocketController = require('./controllers/websocket');
+
+const app = express();
 
 app.use(express.static('public'));
 
